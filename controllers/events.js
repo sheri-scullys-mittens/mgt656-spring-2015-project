efbd55d902f1ff1ee677b3547ec8fec99e58b926
demmerman <david.emmerman@yale.edu>
2015-02-03 20:53:14 +0000

    fixed events button that was added earlier

--- controllers/events.js
+++ controllers/events.js
@@ -62,6 +62,7 @@ function saveEvent(request, response){
 
   if (validator.isLength(request.body.title, 5, 50) === false) {
     contextData.errors.push('Your title should be between 5 and 100 letters.');
+    contextData.title = "Create New Event";
   }
 
 
