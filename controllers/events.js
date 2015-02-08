214212fe25620e67b00794affb570771e7f8abe5
demmerman <david.emmerman@yale.edu>
2015-02-08 01:00:23 +0000

    fixed event id for new events

--- controllers/events.js
+++ controllers/events.js
@@ -103,6 +103,7 @@ function saveEvent(request, response){
 
   if (contextData.errors.length === 0) {
     var newEvent = {
+      id: events.length;
       title: request.body.title,
       location: request.body.location,
       image: request.body.image,
