aef2cc645212b64871e5102d48c0b4c165ac5658
demmerman <david.emmerman@yale.edu>
2015-02-09 19:01:14 +0000

    added id tags to events list

--- controllers/events.js
+++ controllers/events.js
@@ -8,20 +8,6 @@ var validator = require('validator');
 // used a first.
 //
 var allowedDateInfo = {
-  /*months: {
-    0: 'January',
-    1: 'February',
-    2: 'March',
-    3: 'April',
-    4: 'May',
-    5: 'June',
-    6: 'July',
-    7: 'August',
-    8: 'September',
-    9: 'October',
-    10: 'November',
-    11: 'December'
-  },*/
   months: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
   minutes: [0, 30],
   hours: [
