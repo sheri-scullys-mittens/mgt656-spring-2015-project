09891a801535479b1a23ed0a884b9eaeb485d95a
Gordon Schoenfeld <gordon.schoenfeld@gmail.com>
2015-02-11 18:50:12 +0000

    added proper date list

--- controllers/events.js
+++ controllers/events.js
@@ -14,7 +14,12 @@ var allowedDateInfo = {
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
     12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
   ],
-  year: [2015, 2016]
+  year: [2015, 2016],
+  day: [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+    12, 13, 14, 15, 16, 17, 18, 19, 20, 
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+  ]
 };
 
 /**
@@ -63,7 +68,7 @@ function saveEvent(request, response){
     contextData.errors.push('Your date should be an integer.');
   }
   
-  if (validator.isLength(request.body.day, 1, 31) === false) {
+  if (validator.isIn(request.body.day,allowedDateInfo.day) === false) {
     contextData.errors.push('Your date should be between 1 and 31.');
   }
   
